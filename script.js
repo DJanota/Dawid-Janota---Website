0226bbd616146043354b4be426ddf2bccb2444ef
Dawid Janota <6dawid.janota@wp.pl>
2019-06-07 21:30:40 +0200

    script.js

--- script.js
+++ script.js
@@ -1,16 +1,37 @@
-$(document).ready(function() {
-  "use strict";
-  $(".hexFront, .hexBack").click(function() {
-    $(".hexFront")
-      .css({ transform: "rotateY(0deg)", opacity: 1 })
-      .next()
-      .css({ transform: "rotateY(180deg)", opacity: 0 })
-    
-    $(this)
-      .css({ transform: "rotateY(180deg)", opacity: 0 })
-      .next()
-      .css({ transform: "rotateY(0deg)", opacity: 1 })
-      .end().prev()
-      .css({ transform: "rotateY(0deg)", opacity: 1 });
-  });
+$(document).ready(function () {
+    "use strict";
+    $(".hexFront, .hexBack").click(function () {
+        $(".hexFront")
+            .css({
+                transform: "rotateY(0deg)",
+                opacity: 1
+            })
+            .next()
+            .css({
+                transform: "rotateY(180deg)",
+                opacity: 0
+            })
+
+        $(this)
+            .css({
+                transform: "rotateY(180deg)",
+                opacity: 0
+            })
+            .next()
+            .css({
+                transform: "rotateY(0deg)",
+                opacity: 1
+            })
+            .end().prev()
+            .css({
+                transform: "rotateY(0deg)",
+                opacity: 1
+            });
+    });
+});
+
+$(window).scroll(function () {
+    $('element').animate(function () {
+        //kod przesuwający w css 
+    });
 });
