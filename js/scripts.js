03794c91d3aba1360f4413678007d8aff208d541
Dawid Janota <6dawid.janota@wp.pl>
2019-07-30 14:43:08 +0200

    js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -202,3 +202,14 @@ function WHCloseCookiesWindow() {
 
 window.onload = WHCheckCookies;
 //Cooki Alert
+
+//Button Down
+$(function () {
+    $('a[href*=#]').on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top
+        }, 500, 'linear');
+    });
+});
+//Button Down
